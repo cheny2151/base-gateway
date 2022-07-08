69a3f87dd526f1c7f832253cca765293d1e72579
chenyi5 <chenyi5@joyy.com>
2022-07-08 11:10:58 +0800

    更新ExchangeResponseUtils.java

--- src/main/java/com/chenyi/base/gateway/utils/ExchangeResponseUtils.java
+++ src/main/java/com/chenyi/base/gateway/utils/ExchangeResponseUtils.java
@@ -2,14 +2,19 @@ package com.chenyi.base.gateway.utils;
 
 import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.rewrite.CachedBodyOutputMessage;
+import org.springframework.cloud.gateway.support.BodyInserterContext;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ReactiveHttpOutputMessage;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -54,6 +59,56 @@ public class ExchangeResponseUtils {
                         return mono.then(Mono.just(bufferFactory.wrap(bodyStr.getBytes())));
                     });
                     return super.writeWith(modifyBody);
+                }
+                // if body is not a flux. never got there.
+                return super.writeWith(body);
+            }
+        };
+        // replace response with decorator
+        return chain.filter(exchange.mutate().response(decoratedResponse).build());
+    }
+
+
+    /**
+     * 消费响应结果并重写响应
+     *
+     * @param exchange  ServerWebExchange
+     * @param chain     GatewayFilterChain
+     * @param applyFunc 重写的逻辑
+     * @return Mono<Void>
+     */
+    public static Mono<Void> rewrite(ServerWebExchange exchange, GatewayFilterChain chain, Function<String, Mono<String>> applyFunc) {
+        ServerHttpResponse originalResponse = exchange.getResponse();
+        ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+            @Override
+            public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                if (body instanceof Flux) {
+                    Flux<String> modifiedBody = Flux.from(body).buffer().flatMap(dataBuffers -> {
+                        // probably should reuse buffers
+                        String bodyStr = dataBuffers.stream().map(dataBuffer -> {
+                            byte[] content = new byte[dataBuffer.readableByteCount()];
+                            dataBuffer.read(content);
+                            //释放掉内存
+                            DataBufferUtils.release(dataBuffer);
+                            return new String(content, StandardCharsets.UTF_8);
+                        }).collect(Collectors.joining());
+                        Mono<String> mono = applyFunc.apply(bodyStr);
+                        return mono.defaultIfEmpty(bodyStr);
+                    });
+                    BodyInserter<Flux<String>, ReactiveHttpOutputMessage> bodyInserter = BodyInserters.fromPublisher(modifiedBody, String.class);
+                    CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange,
+                            exchange.getResponse().getHeaders());
+                    return bodyInserter.insert(outputMessage, new BodyInserterContext()).then(Mono.defer(() -> {
+                        ServerHttpResponse delegate = getDelegate();
+                        Mono<DataBuffer> messageBody = DataBufferUtils.join(outputMessage.getBody());
+                        HttpHeaders headers = delegate.getHeaders();
+                        if (!headers.containsKey(HttpHeaders.TRANSFER_ENCODING)
+                                || headers.containsKey(HttpHeaders.CONTENT_LENGTH)) {
+                            messageBody = messageBody.doOnNext(data -> headers.setContentLength(data.readableByteCount()));
+                        }
+                        // TODO: fail if isStreamingMediaType?
+                        return delegate.writeWith(messageBody);
+                    }));
                 }
                 // if body is not a flux. never got there.
                 return super.writeWith(body);
